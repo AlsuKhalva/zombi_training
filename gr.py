b495afad0cc06b277cd27b58c4659e44722d66fc
Alsu Khalva <khalva@mail.ru>
2019-06-05 03:13:29 +0300

    вспомогательный класс

---
+++ gr.py
@@ -0,0 +1,18 @@
+class Group:
+
+    def __init__(self, first_name, middle_name, last_name, company, company_address, phone, mobil, email, day,
+                     month, year, home_address, home_number, notes):
+        self.first_name=first_name
+        self.middle_name=middle_name
+        self.last_name=last_name
+        self.company=company
+        self.company_address=company_address
+        self.phone=phone
+        self.mobil=mobil
+        self.email=email
+        self.day=day
+        self.month=month
+        self.year=year
+        self.home_address=home_address
+        self.home_number=home_number
+        self.notes=notes
