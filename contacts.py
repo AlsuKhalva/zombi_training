fdfd7979759962ebc809bbe89b618ab2eb846ff9
Alsu Khalva <khalva@mail.ru>
2019-06-08 21:57:07 +0300

    упростили методы

--- contacts.py
+++ contacts.py
@@ -4,7 +4,7 @@ from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest
-from gr import Group
+from cont_group import Group
 
 class Contacts(unittest.TestCase):
     def setUp(self):
@@ -12,24 +12,24 @@ class Contacts(unittest.TestCase):
         self.wd.implicitly_wait(30)
 
     def test_contacts(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd)
-        self.add_new_contact(wd)
-        self.contact_form(wd, Group(first_name="aaa", middle_name="aaaaaaa", last_name="aaa", company="aa", company_address="hdhdh", phone="99999999", mobil="999999999", email="kaka@mail.ru", day="10",
+        self.login()
+        self.contact_form(Group(first_name="aaa", middle_name="aaaaaaa", last_name="aaa", company="aa", company_address="hdhdh", phone="99999999", mobil="999999999", email="kaka@mail.ru", day="10",
                           month="April", year="1990", home_address="nndndnd x", home_number="1", notes="1"))
-        self.return_to_home_page(wd)
-        self.logout(wd)
+        self.logout()
 
-    def logout(self, wd):
+    def logout(self):
+        wd = self.wd
         wd.find_element_by_link_text("Logout").click()
 
-    def return_to_home_page(self, wd):
+    def return_to_home_page(self):
+        wd = self.wd
         wd.find_element_by_xpath(
             "(//input[@name='submit'])[2]").click()
         wd.find_element_by_link_text("home page").click()
 
-    def contact_form(self, wd, Group):
+    def contact_form(self, Group):
+        wd = self.wd
+        self.add_new_contact()
         # name , company
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
@@ -80,11 +80,15 @@ class Contacts(unittest.TestCase):
         wd.find_element_by_name("notes").click()
         wd.find_element_by_name("notes").clear()
         wd.find_element_by_name("notes").send_keys(Group.notes)
+        self.return_to_home_page()
 
-    def add_new_contact(self, wd):
+    def add_new_contact(self):
+        wd = self.wd
         wd.find_element_by_link_text("add new").click()
 
-    def login(self, wd, login="admin", password="secret"):
+    def login(self, login="admin", password="secret"):
+        wd = self.wd
+        self.open_home_page()
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(login)
@@ -92,7 +96,8 @@ class Contacts(unittest.TestCase):
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
-    def open_home_page(self, wd):
+    def open_home_page(self):
+        wd = self.wd
         wd.get("http://localhost/addressbook/")
 
     def is_element_present(self, how, what):
