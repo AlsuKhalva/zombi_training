ae041220f868226ec4757115514337bc43a7bc38
Alsu Khalva <khalva@mail.ru>
2019-06-08 21:23:46 +0300

    Фикстура. Функции выведены в отдельный класс

--- hw_application.py
+++ hw_application.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
 
 
 class Application:
@@ -50,6 +52,20 @@ class Application:
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
+
+    def is_element_present(self, how, what):
+        try:
+            self.wd.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
+        return True
+
+    def is_alert_present(self):
+        try:
+            self.wd.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
+        return True
 
     def destroy(self):
         self.wd.quit()
