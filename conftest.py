19549c9fd09fcb463f62319170aea874926af6d5
Alsu Khalva <khalva@mail.ru>
2019-06-10 00:29:31 +0300

    Написание теста на удаление группы.

--- conftest.py
+++ conftest.py
@@ -2,7 +2,6 @@ import pytest
 from fixture.hw_application import Application
 
 
-
 @pytest.fixture(scope="session")
 def app(request):
     fixture = Application()
--- fixture/hw_application.py
+++ fixture/hw_application.py
@@ -33,4 +33,3 @@ class Application:
 
     def destroy(self):
         self.wd.quit()
-
