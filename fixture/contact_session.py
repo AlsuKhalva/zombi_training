6cdd22dff6f5788d7fceabb41711327c36fffef0
Alsu Khalva <khalva@mail.ru>
2019-06-13 01:01:12 +0300

    Тест. Правки

--- fixture/contact_session.py
+++ fixture/contact_session.py
@@ -7,12 +7,11 @@ class SessionHelper:
         wd = self.app.wd
         wd.find_element_by_link_text("Logout").click()
 
-    def login(self, login="admin", password="secret"):
+    def login(self, username, password):
         wd = self.app.wd
         self.app.open_home_page()
         wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(login)
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
--- test/test_contact.py
+++ test/test_contact.py
@@ -1,10 +1,19 @@
 # -*- coding: utf-8 -*-
-from model.Contact import Contact
+import pytest
+from model.contact import Contact
+from fixture.contact_application import Application
+
+
+@pytest.fixture(scope="session")
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
 def test_add_contact(app):
-    app.session.login()
+    app.session.login(username="admin", password="secret")
     app.form.create(Contact(first_name="aaa", middle_name="aaaaaaa", last_name="aaa", company="aa", company_address="hdhdh", phone="99999999", mobil="999999999", email="kaka@mail.ru", day="10",
-                               month="April", year="1990", home_address="nndndnd x", home_number="1", notes="1"))
+                            month="April", year="1990", home_address="nndndnd x", home_number="1", notes="1"))
     app.session.logout()
 
