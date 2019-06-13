e6f1a8768d437a3951930805acb891e57fc5bc21
Alsu Khalva <khalva@mail.ru>
2019-06-13 22:58:03 +0300

    Правки в названии тестов

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -1,4 +1,4 @@
-def test_delete_first_group(app):
+def test_edit_first_contact(app):
     app.session.login(username="admin", password="secret")
     app.form.edit_first_contact()
     app.session.logout()
