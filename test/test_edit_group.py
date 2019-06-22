0e039add8f2717e5387d986c68340a284398044c
Alsu Khalva <khalva@mail.ru>
2019-06-22 23:03:33 +0300

    Оптимизация ввода данных формы для групп

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,7 +1,13 @@
 from model.hw_group import Group
 
 
-def test_edit_first_group(app):
+def test_edit_first_group_name(app):
     app.session.login(username="admin", password="secret")
-    app.group.edit_first_group(Group(name="No", header="No", footer="No"))
+    app.group.edit_first_group(Group(name="ZUMZUM"))
+    app.session.logout()
+
+
+def test_edit_first_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.edit_first_group(Group(header="Bebbb"))
     app.session.logout()
