f6bdc8219722daae8e7fdcdc5da7042f974a7456
Alsu Khalva <khalva@mail.ru>
2019-06-23 18:52:49 +0300

    Валидация фикстуры перед каждым тестом

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -2,12 +2,8 @@ from model.hw_group import Group
 
 
 def test_edit_first_group_name(app):
-    app.session.login(username="admin", password="secret")
     app.group.edit_first_group(Group(name="ZUMZUM"))
-    app.session.logout()
 
 
 def test_edit_first_group_header(app):
-    app.session.login(username="admin", password="secret")
     app.group.edit_first_group(Group(header="Bebbb"))
-    app.session.logout()
--- test/test_hw_group.py
+++ test/test_hw_group.py
@@ -4,12 +4,8 @@ from model.hw_group import Group
 
 
 def test_hw_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="Zombi", header="zombi", footer="zombi"))
-    app.session.logout()
 
 
 def test_empty_hw_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="", header="", footer=""))
-    app.session.logout()
