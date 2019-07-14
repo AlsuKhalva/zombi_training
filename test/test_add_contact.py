876bd81a8b1dce9ef32fa53115b834da9a71d153
Alsu Khalva <khalva@mail.ru>
2019-07-14 23:01:34 +0300

    Параметризированные тесты для создания групп и контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,13 +1,33 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import random
+import string
 
 
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    symbol = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbol) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Contact(first_name="", last_name="", email="", email2="", email3="",
+                   day="", month="", year="", notes="",
+                   homephone="", mobilephone="", workphone="", secondaryphone="",
+                   address="", address2="")] + [
+    Contact(first_name=random_string("aaa", 10), last_name=random_string("aaa", 10),
+            email=random_string("kaka@mail.ru", 10), email2=random_string("byby@bb.ru", 10),
+            email3=random_string("mmm@ya.ru", 10), day=random_string("10", 10),
+            month=random_string("April", 10), year=random_string("1990", 10), notes=random_string("1", 10),
+            homephone=random_string("12399", 10), mobilephone=random_string("12345", 10),
+            workphone=random_string("123456", 10), secondaryphone=random_string("212121", 10),
+            address=random_string("Moscow", 10), address2=random_string("Paris", 10))
+    for i in range(3)
+    ]
+
+
+@pytest.mark.parametrize("user", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, user):
     old_contacts_list = app.form.get_contacts_list()
-    user = Contact(first_name="aaa", last_name="aaa", email="kaka@mail.ru", email2="byby@bb.ru", email3="mmm@ya.ru",
-                   day="10", month="April", year="1990", notes="1",
-                   homephone="12399", mobilephone="12345", workphone="123456", secondaryphone="212121",
-                   address="Moscow", address2="Paris")
     app.form.create(user)
     assert len(old_contacts_list) + 1 == app.form.count()
     new_contacts_list = app.form.get_contacts_list()
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,11 +1,25 @@
 # -*- coding: utf-8 -*-
 
 from model.hw_group import Group
+import pytest
+import random
+import string
 
 
-def test_hw_group(app):
+def random_string(prefix, maxlen):
+    symbol = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbol) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Group(name="", header="", footer="")] + [
+    Group(name=random_string("Zombi", 10), header=random_string("zombi", 20), footer=random_string("zombi", 20))
+    for i in range(5)
+    ]
+
+
+@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
+def test_hw_group(app, group):
     old_groups = app.group.get_group_list()
-    group = Group(name="Zombi", header="zombi", footer="zombi")
     app.group.create(group)
     assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()
