659e92bcbdc9044866ff869f70b70ac83b942dc5
Alsu Khalva <khalva@mail.ru>
2019-07-15 00:27:00 +0300

    Опции командной строки, позволяющие указывать тип браузера и адрес

--- fixture/application.py
+++ fixture/application.py
@@ -8,11 +8,19 @@ from fixture.contact_form import FormHelper
 
 class Application:
 
-    def __init__(self):
-        self.wd = webdriver.Firefox()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd == webdriver.Ie()
+        else:
+            raise ValueError("Такого браузера нет %s" % browser)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.form = FormHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -24,8 +32,8 @@ class Application:
     def open_home_page(self):
         wd = self.wd
         if not (wd.current_url.endswith("/addressbook/") and len(wd.find_elements_by_name("searchstring")) > 0):
-            wd.get("http://localhost/addressbook/")
-
+            wd.get(self.base_url)
+            
     def is_element_present(self, how, what):
         try:
             self.wd.find_element(by=how, value=what)
